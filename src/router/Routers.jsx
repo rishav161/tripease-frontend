cf2f7c0f77eb68227ae009cd565a5b9d2e8a4b95
Rishav <rishavjaiswal864@gmail.com>
2025-11-20 12:15:18 +0530

    first commit

---
+++ src/router/Routers.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom"; // Change BrowserRouter to Router
+import Home from "../pages/Home";
+import Tours from "../pages/Tours";
+import TourDetails from "../pages/TourDetails";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import SearchResultList from "../pages/SearchResultList";
+import ThankYou from "../pages/ThankYou";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/tours/:id" element={<TourDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+      <Route path="/search" element={<SearchResultList />} />
+    </Routes>
+  );
+};
+
+export default Routers;
